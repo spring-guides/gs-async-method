eaa9ec2b086d880e34bbcc2583c32ca96eaadb36
Greg Turnquist <gturnquist@gopivotal.com>
2013-07-17 15:01:04 -0400

    Built application to demonstrate asynchronous method calls

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,50 @@
+package hello;
+
+import java.util.concurrent.Future;
+
+import org.springframework.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.bootstrap.CommandLineRunner;
+import org.springframework.bootstrap.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAsync
+@ComponentScan
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	FacebookLookupService facebookLookupService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Start the clock
+		long start = System.currentTimeMillis();
+		
+		// Kick of multiple, asynchronous lookups
+		Future<Page> page1 = facebookLookupService.findPage("GoPivotal");
+		Future<Page> page2 = facebookLookupService.findPage("SpringSource");
+		Future<Page> page3 = facebookLookupService.findPage("CloudFoundry");
+		Future<Page> page4 = facebookLookupService.findPage("SpringFramework");
+		
+		// Wait until they are all done
+		while (!(page1.isDone() && page2.isDone() && page3.isDone() && page4.isDone())) {
+			Thread.sleep(10);
+		}
+		
+		// Print results, including elapsed time
+		System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
+		System.out.println(page1.get());
+		System.out.println(page2.get());
+		System.out.println(page3.get());
+		System.out.println(page4.get());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ complete/src/main/java/hello/FacebookLookupService.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.concurrent.Future;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class FacebookLookupService {
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@Async
+	public Future<Page> findPage(String page) throws InterruptedException {
+		System.out.println("Looking up " + page);
+		Page results = restTemplate.getForObject("http://graph.facebook.com/" + page, Page.class);
+		Thread.sleep(1000L);
+		return new AsyncResult<Page>(results);
+	}
+	
+}
