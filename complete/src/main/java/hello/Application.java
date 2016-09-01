c035ab3cc520d5094f7226b14e8ba3efa939971d
Stephane Nicoll <snicoll@pivotal.io>
2016-09-01 10:52:18 +0200

    Polish
    This commit polishes the code and use more feature of Spring Boot. The
    `RestTemplate` is created via the builder and the sample is executed via
    a CommandLineRunner.
    
    Also, rather than using the default executor, this sample now shows how
    a custom executor can be configured.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,44 +1,30 @@
 package hello;
 
-import java.util.concurrent.Future;
+import java.util.concurrent.Executor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    GitHubLookupService gitHubLookupService;
-
-    @Override
-    public void run(String... args) throws Exception {
-        // Start the clock
-        long start = System.currentTimeMillis();
-
-        // Kick of multiple, asynchronous lookups
-        Future<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
-        Future<User> page2 = gitHubLookupService.findUser("CloudFoundry");
-        Future<User> page3 = gitHubLookupService.findUser("Spring-Projects");
-
-        // Wait until they are all done
-        while (!(page1.isDone() && page2.isDone() && page3.isDone())) {
-            Thread.sleep(10); //10-millisecond pause between each check
-        }
-
-        // Print results, including elapsed time
-        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
-        System.out.println(page1.get());
-        System.out.println(page2.get());
-        System.out.println(page3.get());
-    }
+public class Application extends AsyncConfigurerSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
     }
 
 }
--- complete/src/main/java/hello/GitHubLookupService.java
+++ complete/src/main/java/hello/GitHubLookupService.java
@@ -2,6 +2,9 @@ package hello;
 
 import java.util.concurrent.Future;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
@@ -10,15 +13,22 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GitHubLookupService {
 
-    RestTemplate restTemplate = new RestTemplate();
+    private static final Logger logger = LoggerFactory.getLogger(GitHubLookupService.class);
+
+    private final RestTemplate restTemplate;
+
+    public GitHubLookupService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     @Async
     public Future<User> findUser(String user) throws InterruptedException {
-        System.out.println("Looking up " + user);
-        User results = restTemplate.getForObject("https://api.github.com/users/" + user, User.class);
+        logger.info("Looking up " + user);
+        String url = String.format("https://api.github.com/users/%s", user);
+        User results = restTemplate.getForObject(url, User.class);
         // Artificial delay of 1s for demonstration purposes
         Thread.sleep(1000L);
-        return new AsyncResult<User>(results);
+        return new AsyncResult<>(results);
     }
 
 }
