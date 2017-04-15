3b26afc06a4dc9143bb86a7aab4b85c7da1d1488
Zoltan Altfatter <altfatterz@gmail.com>
2017-04-15 13:12:08 +0200

    move from Future to CompletableFuture

--- complete/src/main/java/hello/GitHubLookupService.java
+++ complete/src/main/java/hello/GitHubLookupService.java
@@ -1,14 +1,13 @@
 package hello;
 
-import java.util.concurrent.Future;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class GitHubLookupService {
@@ -22,13 +21,13 @@ public class GitHubLookupService {
     }
 
     @Async
-    public Future<User> findUser(String user) throws InterruptedException {
+    public CompletableFuture<User> findUser(String user) throws InterruptedException {
         logger.info("Looking up " + user);
         String url = String.format("https://api.github.com/users/%s", user);
         User results = restTemplate.getForObject(url, User.class);
         // Artificial delay of 1s for demonstration purposes
         Thread.sleep(1000L);
-        return new AsyncResult<>(results);
+        return CompletableFuture.completedFuture(results);
     }
 
 }
