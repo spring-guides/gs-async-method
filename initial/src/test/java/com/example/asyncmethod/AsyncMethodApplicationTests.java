855a881f2637b87952453bf50de636cb031b6a55
Jay Bryant <jbryant@pivotal.io>
2019-10-21 15:29:59 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2, which required replacing the Gradle Wrapper. Note that there are no tests in the /complete branch.

---
+++ initial/src/test/java/com/example/asyncmethod/AsyncMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asyncmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
